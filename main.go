96656c8d7f292cd44e7ca1c9d2c5d626bce8ea81
gandatadyo <gandatadyosurya@yahoo.com>
2018-10-31 06:45:02 +0700

    Update 31 Okt 2018

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,11 +11,17 @@ import (
 var cin *gin.Engine
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	cin = gin.Default()
 	cin.Delims("{$", "}")
 	cin.LoadHTMLGlob("src/templates/*")
 	cin.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home.html", gin.H{})
 	})
-	cin.Run(":1000")
+	cin.Run(":" + port)
 }
