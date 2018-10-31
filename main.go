1496235239edf67f32792aef34215aeee8ca27a2
gandatadyo <gandatadyosurya@yahoo.com>
2018-11-01 06:11:54 +0700

    Update 1 Nov 2018

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
@@ -13,73 +12,138 @@ import (
 var cin *gin.Engine
 
 func main() {
-	// bot, err := linebot.New(
-	// 	os.Getenv("977b5cf3276bce134af72ad4e6e45707"), //CHANNEL_SECRET
-	// 	os.Getenv("nFAf7VS/RulMCFabBKJfgIHvDxfKDpENufTZ4lWQ1M0AGUTKMJG+bVMw+uA/Twon2qYS/EDq/B3AHy/JnVZSnq25RG7SPpEF8QL87YFqDzzne6ZHYA/bhmu6jE3k6/1DG/+a4sp5Bo3+7ZOAwoSLbQdB04t89/1O/w1cDnyilFU="), //CHANNEL_TOKEN
-	// )
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
+	// init bot
 	client := &http.Client{}
-	bot, err := linebot.New("<16de7664bba6666afdbec1f444b63fd6>", "<wFg+qNbyo6tP4vlx6av2hmNB3Z5xbLNLH0SFloVxH7xuIxOb9qKaH1X1P75XhAvx2qYS/EDq/B3AHy/JnVZSnq25RG7SPpEF8QL87YFqDzxbgu3+EQ2jGTgqC1Ar2trr/C6OfBW27swPBVLxmUU5jVGUYhWQfeY8sLGRXgo3xvw=>", linebot.WithHTTPClient(client))
-
+	bot, err := linebot.New("<24ee6cbebfd41457136563b41f8184d6>", "<LZANYzm90f3h6eUyUl4PNce0sTjKQeojp7/E+F3NAyBW/ltsD3UCqsZdTxcyiEk+76+IXhfmzPKjoP16JDCMnzVI/EI5nQarf3h5ngNBDctTepFQAhtP/25yj5EV72OkNtIFPYxzgQPLwssXgQ8gpgdB04t89/1O/w1cDnyilFU=>", linebot.WithHTTPClient(client))
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Not Work 1")
 	}
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
+	// init port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1000"
+		fmt.Println("$PORT must be set")
+	}
+
+	// init gin
+	cin = gin.Default()
+	cin.Delims("{$", "}")
+	cin.LoadHTMLGlob("src/templates/*")
+
+	// event all
+	cin.POST("/callback", func(c *gin.Context) {
+		fmt.Println("Work start POST")
+		events, err := bot.ParseRequest(c.Request)
+		fmt.Println("Work 1.1")
 		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
+			fmt.Println("Error 1.1")
 		}
 		for _, event := range events {
+			fmt.Println("Work 1.2")
+
+			fmt.Println(event.Source.UserID)
+			fmt.Println(event.Source.GroupID)
+			fmt.Println(event.Source.RoomID)
+			fmt.Println(event.ReplyToken)
+
 			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
+				fmt.Println("Work EventTypeMessage")
+
+			}
+			if event.Type == linebot.EventTypeFollow {
+				fmt.Println("Work EventTypeFollow")
+
+			}
+			if event.Type == linebot.EventTypeUnfollow {
+				fmt.Println("Work EventTypeUnfollow")
+
+			}
+			if event.Type == linebot.EventTypeJoin {
+				fmt.Println("Work EventTypeJoin")
+
+			}
+			if event.Type == linebot.EventTypeLeave {
+				fmt.Println("Work EventTypeLeave")
+
+			}
+			if event.Type == linebot.EventTypeBeacon {
+				fmt.Println("Work EventTypeBeacon")
+
 			}
 		}
+
+		// var valheader = c.Request.Header.Get("valheader")
+		// buf := make([]byte, 1024)
+		// num, _ := c.Request.Body.Read(buf)
+		// valbody := string(buf[0:num])
+		// c.HTML(http.StatusOK, "home.html", gin.H{"valheader": valheader, "valbody": string(valbody)})
 	})
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		log.Fatal(err)
-	}
+	cin.GET("/callback", func(c *gin.Context) {
+		fmt.Println("Work start GET")
+		events, err := bot.ParseRequest(c.Request)
+		fmt.Println("Work 1.1")
+		if err != nil {
+			fmt.Println("Error 1.1")
+		}
+		for _, event := range events {
+			fmt.Println("Work 1.2")
 
-	// port := os.Getenv("PORT")
+			fmt.Println(event.Source.UserID)
+			fmt.Println(event.Source.GroupID)
+			fmt.Println(event.Source.RoomID)
+			fmt.Println(event.ReplyToken)
 
-	// if port == "" {
-	// 	port = "1000"
-	// 	// log.Fatal("$PORT must be set")
-	// }
+			if event.Type == linebot.EventTypeMessage {
+				fmt.Println("Work EventTypeMessage")
 
-	// cin = gin.Default()
-	// cin.Delims("{$", "}")
-	// cin.LoadHTMLGlob("src/templates/*")
-	// cin.POST("/", func(c *gin.Context) {
-	// 	var valheader = c.Request.Header.Get("valheader")
+			}
+			if event.Type == linebot.EventTypeFollow {
+				fmt.Println("Work EventTypeFollow")
 
-	// 	buf := make([]byte, 1024)
-	// 	num, _ := c.Request.Body.Read(buf)
-	// 	valbody := string(buf[0:num])
-	// 	c.HTML(http.StatusOK, "home.html", gin.H{"valheader": valheader, "valbody": string(valbody)})
-	// })
-	// cin.GET("/", func(c *gin.Context) {
-	// 	var valheader = c.Request.Header.Get("valheader")
+			}
+			if event.Type == linebot.EventTypeUnfollow {
+				fmt.Println("Work EventTypeUnfollow")
 
-	// 	// c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
-	// 	c.HTML(http.StatusOK, "home.html", gin.H{"valheader": valheader, "valbody": " THIS IS METHOD GET"})
-	// })
-	// cin.Run(":" + port)
-	fmt.Println("running apps bos")
+			}
+			if event.Type == linebot.EventTypeJoin {
+				fmt.Println("Work EventTypeJoin")
+
+			}
+			if event.Type == linebot.EventTypeLeave {
+				fmt.Println("Work EventTypeLeave")
+
+			}
+			if event.Type == linebot.EventTypeBeacon {
+				fmt.Println("Work EventTypeBeacon")
+
+			}
+		}
+
+		// var valheader = c.Request.Header.Get("valheader")
+		// // c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
+		// c.HTML(http.StatusOK, "home.html", gin.H{"valheader": valheader, "valbody": " THIS IS METHOD GET"})
+	})
+
+	cin.GET("/pushmessage", func(c *gin.Context) {
+		fmt.Println("Work start PUSH MESSAGE")
+
+		events, err := bot.ParseRequest(c.Request)
+		fmt.Println("Work 1.1")
+		if err != nil {
+			fmt.Println("Error 1.1")
+		}
+		for _, event := range events {
+			fmt.Println(event.Source.UserID)
+			fmt.Println(event.Source.GroupID)
+			fmt.Println(event.Source.RoomID)
+			fmt.Println(event.ReplyToken)
+			if _, err := bot.PushMessage(event.Source.GroupID, linebot.NewTextMessage("hello")).Do(); err != nil {
+				fmt.Println("Error 1.2")
+			}
+		}
+
+	})
+
+	cin.Run(":" + port)
 }
